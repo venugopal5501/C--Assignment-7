3dccb7d1e8837f83386bf3a509c57defe0022f58
venugopal5501 <venugopal14082002@gmail.com>
2024-09-20 18:44:19 +0530

    delegate

--- 4.delegate.cs
+++ 4.delegate.cs
@@ -1,8 +1,18 @@
 using System;
 class function
 {
+    delegate int fee(int n);
+    public static int feets(int n){
+
+
+    }
     public static void Main(string[] args)
     {
-        
+        Console.Write("Enter the feet measurement : ");
+        int feet = Convert.ToInt32(Console.ReadLine());
+        fee f=new fee(feets);
+        int a;
+        feets(feet); 
+
     }
 }
